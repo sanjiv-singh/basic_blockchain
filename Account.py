bfdf20146138e974ea8699d1e5101df5db721282
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-02-12 20:44:07 +0530

    private and public key generation and conversion to PEM format

--- Account.py
+++ Account.py
@@ -38,7 +38,20 @@ class Account:
     def __generate_key_pair(self):
         # Implement key pair generation logic
         # Convert them to pem format strings and store in the class attributes already defined
-        pass
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        self._private_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        public_key = private_key.public_key()
+        self._public_pem = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
 
     def create_transaction(self, receiver_id, value, tx_metadata=''):
         nonce = self._nonce + 1
